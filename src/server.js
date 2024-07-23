3993bf3da00afb5c55fa4d1c519b2809095ca99d
Renan Gimenez <renangiimenez@gmail.com>
2024-07-23 10:06:52 -0300

    fix: update PrismaClient require

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { PrismaClient } = require("./prisma/generated/client");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
