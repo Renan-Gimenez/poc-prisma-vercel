78f00bdcec36b22180192f52efe7792303cca632
Renan Gimenez <renangiimenez@gmail.com>
2024-07-23 09:54:06 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { PrismaClient } = require("./prisma/generated/client");
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.json(users);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
